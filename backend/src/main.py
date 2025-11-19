9d20e5d767e33390a5bbdeda662d530084fcefff
ElPitagoras14 <jonfrgar@espol.edu.ec>
2025-11-18 21:28:40 -0500

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -105,9 +105,9 @@ async def scalar_html():
 
 
 def start():
-    logger.info("Server starting on port 4002")
+    logger.info("Server starting on port 8000")
 
-    uvicorn.run(app="main:app", host="0.0.0.0", port=4002, reload=False)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=False)
 
 
 if __name__ == "__main__":
